495eb9168e1982d5085c2ecd80960e4b6bf99bd6
webshunter <webshunternet@gmail.com>
2025-08-10 16:44:19 +0700

    update: change pm2 process name for port 3001 to git-webhook in service.sh

--- service.sh
+++ service.sh
@@ -7,4 +7,22 @@ echo "Menjalankan post-pull script..."
 # Contoh: npm install && npm run build
 npm install --force
 
+	# Fungsi untuk cek dan jalankan service jika belum berjalan
+check_and_start() {
+	PORT=$1
+	SCRIPT=$2
+	NAME=$3
+	if lsof -i :$PORT | grep LISTEN > /dev/null; then
+		echo "$NAME sudah berjalan di port $PORT."
+	else
+		echo "$NAME belum berjalan. Menjalankan dengan pm2..."
+		pm2 start $SCRIPT --name $NAME
+	fi
+}
+
+# Cek dan jalankan masing-masing service
+check_and_start 3000 apigetway.js apigetway
+check_and_start 3001 webhook-service.js git-webhook
+check_and_start 3002 landing.js landing
+
 echo "Selesai menjalankan post-pull script."
